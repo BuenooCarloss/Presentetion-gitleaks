2c2b44c767251db7b1999c48074f604734549d79
Cadu <carlos.e.bueno@hotmail.com>
2025-03-06 11:17:08 -0300

    test

---
+++ Secrets/Program.cs
@@ -0,0 +1,17 @@
+﻿// Exemplo de senha
+senha = 'ccccascscasaac'
+
+// Exemplo de password
+password = 1234
+
+// Exemplo de chave de API genérica
+string apiKey = "api: 98abf8e7f56a129bfe902e8bc9294d84"; // API Key (exemplo genérico)
+
+// Exemplo de token do GitHub
+string githubToken = "ghp_abcd1234efgh5678ijkl91011mnopqrst"; // GitHub Personal Access Token
+
+// Exemplo de chave de acesso da AWS
+string awsAccessKey = "AKIAIOSFODNN7EXAMPLE"; // AWS Access Key ID
+
+// Exemplo de chave secreta da AWS
+string awsSecretKey = "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"; // AWS Secret Access Key
